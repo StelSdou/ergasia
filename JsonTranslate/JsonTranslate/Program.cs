6c7a0a99ad257cfc8c367c91aea37c45ce7336a2
StelSdou <stelsd05@gmail.com>
2024-12-24 23:21:26 +0200

    first

---
+++ JsonTranslate/JsonTranslate/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTranslate
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //https://api.mymemory.translated.net/get?q=Hello&langpair=en|el
+            // Η βάση URL της API
+            string url = "https://api.mymemory.translated.net/get";
+            string sourceLang = "en"; // Αγγλικά
+            string targetLang = "el"; // gr
+
+            // Παράμετροι για την κλήση της API (π.χ. κείμενο προς μετάφραση και γλώσσες)
+            string textToTranslate = "It all started with Pandora box. You probably already know the story of Pandora, but you might not know what happened next. Pandora's box caused more problems!";
+
+            // Δημιουργία HTTP client
+            using (HttpClient client = new HttpClient())
+            {
+                // Δημιουργία πλήρους URL με τα query parameters
+                string requestUrl = $"{url}?q={Uri.EscapeDataString(textToTranslate)}&langpair={sourceLang}|{targetLang}";
+
+                try
+                {
+                    // Κλήση της API
+                    HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+                    // Βεβαιώσου ότι η κλήση πέτυχε
+                    response.EnsureSuccessStatusCode();
+
+                    // Ανάγνωση του περιεχομένου της απάντησης
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Ανάλυση της JSON απάντησης και εξαγωγή της μετάφρασης
+                    JObject jsonResponse = JObject.Parse(responseBody);
+                    string translatedText = jsonResponse["responseData"]["translatedText"].ToString();
+
+                    // Εμφάνιση μόνο της μετάφρασης
+                    Console.WriteLine("Μετάφραση: " + translatedText);
+                }
+                catch (Exception e)
+                {
+                    // Διαχείριση σφαλμάτων
+                    Console.WriteLine($"Σφάλμα: {e.Message}");
+                }
+            }
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ txtToJson/txtToJson/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace txtToJson
+{
+    class jsonconvert
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string History { get; set; }
+
+        public jsonconvert(int id, string category, string title, string history)
+        {
+            Id = id;
+            Category = category;
+            Title = title;
+            History = history;
+        }
+    }
+
+    internal class Program
+    {
+        private static string path = "Data.json";
+        static void Main(string[] args)
+        {
+            List<jsonconvert> list = new List<jsonconvert>();
+
+            string filePath = Console.ReadLine();
+
+            if (File.Exists(filePath))
+            {
+                string text = File.ReadAllText(filePath);
+
+                string newText = text.Replace(" ", "_").Replace("\"_", "\" ");
+
+                File.WriteAllText(filePath, newText);
+
+                int id = 0;
+                string cat = "";
+                string tit = "";
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] words = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (words.Length != 0)
+                            if (words[0].ToLower() == "\"category\"")
+                                cat = words[1].Replace("_", " ");
+                            else if (words[0].ToLower() == "\"title\"")
+                                tit = words[1].Replace("_", " ");
+                            else if(words[0].ToLower() == "\"history\"")
+                                list.Add(new jsonconvert(id++, cat, tit, words[1].Replace("_", " ")));
+                    }
+                    Console.WriteLine(id);
+                }
+            }
+
+            File.WriteAllText(path, JsonSerializer.Serialize(list));
+            Console.ReadLine();
+        }
+    }
+}
