e054738896a5438f8921d75fb5aa20eae8618744
StelSdou <stelsd05@gmail.com>
2025-01-16 00:24:25 +0200

    with BattleShip

---
+++ BattleShip/BattleShip/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    internal class Program
+    {
+        // Black table, false
+        private static bool[,] MyShips = new bool[10, 10];
+        // Green table, null
+        private static bool?[,] myHits = new bool?[10, 10];
+
+        // Blue table, false
+        private static bool[,] enemyShips = new bool[10, 10];
+        // Red table, null
+        private static bool?[,] enemyHits = new bool?[10, 10];
+
+        static void Main(string[] args)
+        {
+            Ship submarine = new Ship(2);
+            submarine.SetPosition(MyShips, 3, 5, 0);
+
+            Ship battleship = new Ship(3);
+            battleship.SetPosition(MyShips, 6, 6, 1);
+
+            Ship destroyer = new Ship(4);
+            destroyer.SetPosition(MyShips, 0, 4, 0);
+
+            Ship aircraft_carrier = new Ship(5);
+            aircraft_carrier.SetPosition(MyShips, 1, 3, 1);
+
+            print();
+            Console.ReadLine();
+        }
+
+        static void MyAttack(int x, int y)
+        {
+            if (enemyShips[x, y])
+                myHits[x, y] = true;
+            else
+                myHits[x, y] = false;
+        }
+
+        static void print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    string x = " ";
+                    if (MyShips[i, j])
+                        x = "1";
+                    else
+                        x = " ";
+                    Console.Write(x + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BattleShip/BattleShip/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    internal class Ship
+    {
+        private int shipSize;
+        private int[,] coordinates = new int[2, 2];
+        // Horizontal
+        private int[] direction = { 0, 1 };
+        int sumOfHits = 0;
+        public Ship(int shipSize)
+        {
+            this.shipSize = shipSize;
+        }
+        public void SetPosition(bool[,] ourShips, int x, int y, int d)
+        {
+            SetDirection(d);
+            // start
+            coordinates[0, 0] = x;
+            coordinates[0, 1] = y;
+            // end
+            coordinates[1, 0] = x + (shipSize - 1) * direction[0];
+            coordinates[1, 1] = y + (shipSize - 1) * direction[1];
+            
+            ourShips[x, y] = true;
+            
+            for (int i = 1; i < shipSize; i++)
+                 ourShips[x + i * direction[0], y + i * direction[1]] = true;
+        }
+        private void SetDirection(int d)
+        {
+            this.direction[0] = d;
+            this.direction[1] = 1 - d;
+        }
+        private void SumHits(int sum)
+        {
+            sum++;
+        }
+    }
+}
